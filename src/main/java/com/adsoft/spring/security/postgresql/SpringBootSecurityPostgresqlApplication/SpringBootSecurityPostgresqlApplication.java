676c532a34b834c621f3e1d83c1704383945df96
luis-pseudo <luisangelpr31@gmail.com>
2026-05-27 03:15:20 -0600

    modified:   src/main/java/com/adsoft/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/SpringBootSecurityPostgresqlApplication.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/adsoft/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/SpringBootSecurityPostgresqlApplication.java
+++ src/main/java/com/adsoft/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/SpringBootSecurityPostgresqlApplication.java
@@ -1,13 +1,28 @@
 package com.adsoft.spring.security.postgresql.SpringBootSecurityPostgresqlApplication;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.adsoft.spring.security.postgresql.SpringBootSecurityPostgresqlApplication.models.ERole;
+import com.adsoft.spring.security.postgresql.SpringBootSecurityPostgresqlApplication.models.Role;
+import com.adsoft.spring.security.postgresql.SpringBootSecurityPostgresqlApplication.repository.RoleRepository;
 
 @SpringBootApplication
 public class SpringBootSecurityPostgresqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityPostgresqlApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initRoles(RoleRepository roleRepository) {
+		return args -> {
+			for (ERole roleName : ERole.values()) {
+				roleRepository.findByName(roleName).orElseGet(() -> roleRepository.save(new Role(roleName)));
+			}
+		};
 	}
 
 }
